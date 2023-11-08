5f55829eacca3657f1f79f6891b2136fa9e2177b
WENGELAWI MATHEWOS <MEETWENGEL@GMAIL.COM>
2023-11-07 22:17:36 -0600

    commit

---
+++ sd415/OOP/d22methods/src/calculator.ts
@@ -0,0 +1,15 @@
+export const calculator = {
+    operand1:0,
+    operand2:0,
+    setValues(operand1:number, operand2:number):void{
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    },
+    sum(){
+        return this.operand1 + this.operand2;
+    },
+    mul (){
+        return this.operand1 * this.operand2;
+    }
+};  
+
